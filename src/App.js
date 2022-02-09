73224add0646d4a566fb525ba26b07befe5ed051
BrainGhost <juniorbalamage@gmail.com>
2022-02-09 16:38:24 +0300

    Finish the movies container display, integration of framer-motion

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import { AnimatePresence, motion } from "framer-motion";
+import { useEffect, useState } from "react";
+import Filter from "./Filter";
+import MoviesCard from "./MoviesCard";
 
 function App() {
+  const [dataMovies, setDataMovies] = useState([]);
+  const [filterMovies, setfilterMovies] = useState([]);
+  const [activeGenre, setActiveGenre] = useState(0);
+
+  useEffect(() => {
+    fetchMovies();
+  }, []);
+
+  const fetchMovies = async () => {
+    const data = await fetch(
+      "https://api.themoviedb.org/3/movie/popular?api_key=4408478627d5cddc9f88bc64c5d89b56&language=en-US&page=1"
+    );
+    const movies = await data.json();
+    setDataMovies(movies.results);
+    setfilterMovies(movies.results);
+    console.log(movies.results);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Filter
+        dataMovies={dataMovies}
+        setfilterMovies={setfilterMovies}
+        activeGenre={activeGenre}
+        setActiveGenre={setActiveGenre}
+      />
+      <motion.div layout className="poular_movies">
+        <AnimatePresence>
+          {filterMovies.map((movie) => (
+            <MoviesCard key={movie.id} movie={movie} />
+          ))}
+        </AnimatePresence>
+      </motion.div>
     </div>
   );
 }
